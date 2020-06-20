83d7f3718b27e753762e562e6ac0cf077146b212
Shamil <shamil.sqa@gmail.com>
2020-06-20 00:18:59 -0600

    waiting_game

---
+++ waiting_game.py
@@ -0,0 +1,26 @@
+import random
+import time
+
+print("Waiting game")
+print("Your target time is 4 seconds")
+
+print("---Press ENTER to begin---")
+input()
+
+waitingTime = random.randint(4,4)
+start = time.time()
+input("...Press Enter again after 4 seconds...")
+end = time.time()
+elapsed = end - start
+
+difference = round(waitingTime - elapsed,3)
+
+print("\n" "Elapsed time: " + str(elapsed) + " seconds")
+
+if difference < 0.1:
+    print(str(difference) + " seconds too slow")
+elif difference > 0.1:
+    print(str(difference) + " seconds too fast")
+else:
+    print("You did it")
+
